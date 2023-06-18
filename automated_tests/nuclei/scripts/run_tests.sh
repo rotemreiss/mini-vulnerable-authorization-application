5342007c874db070986737bf9fe73776bd359e32
Rotem Reiss <reiss.r@gmail.com>
2023-06-19 01:33:07 +0300

    Fix run tests script

--- automated_tests/nuclei/scripts/run_tests.sh
+++ automated_tests/nuclei/scripts/run_tests.sh
@@ -2,7 +2,7 @@
 
 # Run our tests.
 # Supports additional arguments to be passed to Nuclei.
-nuclei -t ../templates -u $URL -var token_user=$TOKEN_USER -var token_admin=$TOKEN_ADMIN -json -silent -severity high "$@" > ../results.json
+nuclei -t automated_tests/nuclei/templates -u $URL -var token_user=$TOKEN_USER -var token_admin=$TOKEN_ADMIN -json -silent -severity high "$@" > ../results.json
 
 # Now generate the RBAC matrix
 #python ./utils/rbac-matrix-generator.py -t ../templates -r ../results.json -o ../../rbac_matrix_ui/matrix.json
