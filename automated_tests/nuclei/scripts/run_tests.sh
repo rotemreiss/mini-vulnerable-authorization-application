17f40c056c9ee7c31fad50f97e7ce31e5986022c
Rotem Reiss <reiss.r@gmail.com>
2023-06-19 08:59:22 +0300

    Upload artifact

--- automated_tests/nuclei/scripts/run_tests.sh
+++ automated_tests/nuclei/scripts/run_tests.sh
@@ -4,7 +4,7 @@ URL=http://localhost:3000
 
 # Run our tests.
 # Supports additional arguments to be passed to Nuclei.
-nuclei -t automated_tests/nuclei/templates -u $URL -var token_user=$TOKEN_USER -var token_admin=$TOKEN_ADMIN -j -silent -severity high "$@" > ../results.json
+nuclei -t automated_tests/nuclei/templates -u $URL -var token_user=$TOKEN_USER -var token_admin=$TOKEN_ADMIN -j -silent -severity high "$@" > results.json
 
 # Now generate the RBAC matrix
 #python ./utils/rbac-matrix-generator.py -t ../templates -r ../results.json -o ../../rbac_matrix_ui/matrix.json
