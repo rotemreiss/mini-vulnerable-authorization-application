aadbc77092a3fb3ba95dc1b88e84ababa04f4e9f
Rotem Reiss <reiss.r@gmail.com>
2023-06-26 09:46:05 +0300

    Add Swagger UI and annotations

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ require('dotenv').config();
 const express = require('express');
 const jwt = require('jsonwebtoken');
 const expressJwt = require('express-jwt');
+const swaggerJsdoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
 const fs = require('fs');
 const users = JSON.parse(fs.readFileSync('./data/users.json', 'utf8'));
 
@@ -37,6 +39,77 @@ app.use((err, req, res, next) => {
     next(err);
 });
 
+/**
+ * @swagger
+ * tags:
+ *   - name: Authentication
+ *     description: API endpoints for user authentication
+ */
+
+/**
+ * @swagger
+ * components:
+ *   securitySchemes:
+ *     bearerAuth:
+ *       type: http
+ *       scheme: bearer
+ *       bearerFormat: JWT
+ *   schemas:
+ *     User:
+ *       type: object
+ *       properties:
+ *         username:
+ *           type: string
+ *           description: User's username
+ *         role:
+ *           type: string
+ *           description: User's role
+ *     ErrorResponse:
+ *       type: object
+ *       properties:
+ *         message:
+ *           type: string
+ *           description: Error message
+ */
+
+/**
+ * @swagger
+ * /api/login:
+ *   post:
+ *     summary: User login
+ *     tags: [Authentication]
+ *     requestBody:
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               username:
+ *                 type: string
+ *               password:
+ *                 type: string
+ *             required:
+ *               - username
+ *               - password
+ *     responses:
+ *       200:
+ *         description: Successful login
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 token:
+ *                   type: string
+ *                   description: Generated JWT token
+ *       401:
+ *         description: Invalid credentials
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/ErrorResponse'
+ */
+
 app.post('/api/login', (req, res) => {
     const { username, password } = req.body;
     const user = users.find(user => user.username === username && user.password === password);
@@ -54,11 +127,42 @@ app.post('/api/login', (req, res) => {
     res.json({ token });
 });
 
-
+/**
+ * @swagger
+ * /api/logout:
+ *   post:
+ *     summary: User logout
+ *     tags: [Authentication]
+ *     responses:
+ *       200:
+ *         description: User logged out
+ */
 app.post('/api/logout', (req, res) => {
     res.json({message: 'User logged out'});
 });
 
+/**
+ * @swagger
+ * /api/me:
+ *   get:
+ *     summary: Get user information
+ *     tags: [User]
+ *     security:
+ *       - bearerAuth: []
+ *     responses:
+ *       200:
+ *         description: User information
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/User'
+ *       401:
+ *         description: Unauthorized or invalid user
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/ErrorResponse'
+ */
 app.get('/api/me', (req, res) => {
     if (!req.user) {
         return res.status(401).json({ message: 'You are not authorized' });
@@ -72,6 +176,30 @@ app.get('/api/me', (req, res) => {
     }
 });
 
+/**
+ * @swagger
+ * /api/product:
+ *   get:
+ *     summary: Get product information
+ *     tags: [Product]
+ *     responses:
+ *       200:
+ *         description: Product information
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 name:
+ *                   type: string
+ *                   description: Product name
+ *                 sku:
+ *                   type: string
+ *                   description: Product SKU
+ *                 price:
+ *                   type: number
+ *                   description: Product price
+ */
 app.get('/api/product', (req, res) => {
     res.json({name: 'Dummy Product', sku: 'DUM123', price: 19.99});
 });
@@ -83,15 +211,116 @@ const adminOnly = (req, res) => {
     res.json({ success: true });
 }
 
+/**
+ * @swagger
+ * /api/product:
+ *   post:
+ *     summary: Create a new product (admin only)
+ *     tags: [Product]
+ *     security:
+ *       - bearerAuth: []
+ *     responses:
+ *       200:
+ *         description: Product created
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 success:
+ *                   type: boolean
+ *                   description: Indicates if the product was created successfully
+ *       401:
+ *         description: Unauthorized
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/ErrorResponse'
+ */
 app.post('/api/product', adminOnly);
 
+/**
+ * @swagger
+ * /api/admin:
+ *   get:
+ *     summary: Admin only route
+ *     tags: [Authentication]
+ *     security:
+ *       - bearerAuth: []
+ *     responses:
+ *       200:
+ *         description: Success
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 success:
+ *                   type: boolean
+ *                   description: Indicates if the request was successful
+ *       401:
+ *         description: Unauthorized
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/ErrorResponse'
+ */
 app.get('/api/admin', adminOnly);
 
+/**
+ * @swagger
+ * /api/store/{storeId}:
+ *   get:
+ *     summary: Access a specific store
+ *     tags: [Store]
+ *     security:
+ *       - bearerAuth: []
+ *     parameters:
+ *       - in: path
+ *         name: storeId
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: Store ID
+ *     responses:
+ *       200:
+ *         description: Success
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 success:
+ *                   type: boolean
+ *                   description: Indicates if the request was successful
+ *       401:
+ *         description: Unauthorized
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/ErrorResponse'
+ */
 app.get('/api/store/:storeId', (req, res) => {
     if (!req.user || req.user.store != req.params.storeId) return res.status(401).json({message: 'Unauthorized'});
     res.json({ success: true });
 });
 
 app.listen(3000, () => console.log('App is running on port 3000'));
+
+const options = {
+    swaggerDefinition: {
+        openapi: '3.0.0',
+        info: {
+            title: 'mVAA API',
+            version: '1.0.0',
+        },
+    },
+    apis: ['./app.js'], // Specify the path to your app.js file
+};
+
+const swaggerSpec = swaggerJsdoc(options);
+
+app.use('/api-docs', swaggerUi.serve);
+app.get('/api-docs', swaggerUi.setup(swaggerSpec));
 
 module.exports = app; // export the app
--- rbac_tests/scripts/generate_role_tests.sh
+++ rbac_tests/scripts/generate_role_tests.sh
@@ -4,19 +4,35 @@
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
 # Get the required data to generate the templates.
-read -p "Enter the HTTP method (e.g. GET): " http_method
-read -p "Enter the API name (e.g. product): " api_name
-read -p "Enter the API human readable name (e.g. Product): " api_human_name
-read -p "Enter the API path without the leading slash (e.g. api/product): " api_path
+read -rp "Enter the HTTP method (e.g. GET): " http_method
+while [[ -z $http_method ]]; do
+    echo "HTTP method is required."
+    read -rp "Enter the HTTP method (e.g. GET): " http_method
+done
 
-# @todo Handle errors and make it all actually mandatory + add a verification question.
+read -rp "Enter the API name (e.g. product): " api_name
+while [[ -z $api_name ]]; do
+    echo "API name is required."
+    read -rp "Enter the API name (e.g. product): " api_name
+done
+
+read -rp "Enter the API human readable name (e.g. Product): " api_human_name
+while [[ -z $api_human_name ]]; do
+    echo "API human readable name is required."
+    read -rp "Enter the API human readable name (e.g. Product): " api_human_name
+done
+
+read -rp "Enter the API path without the leading slash (e.g. api/product): " api_path
+while [[ -z $api_path ]]; do
+    echo "API path is required."
+    read -rp "Enter the API path without the leading slash (e.g. api/product): " api_path
+done
 
 echo "[-] Creating the templates directory tree."
 mkdir -p "$SCRIPT_DIR/../templates/$api_path"
 
 echo "[-] Creating the templates."
-for role in admin user
-do
+for role in admin user; do
     tpl_path="$SCRIPT_DIR/../templates/${api_path}/${http_method}-${role}.yaml"
     cp "$SCRIPT_DIR/../base_templates/role-status-based.yaml" "$tpl_path"
     api_id=$(echo "$api_path" | tr '/' '-')
